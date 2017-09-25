00746e89c5797ab3dca9b5f51c29c7c5142fd715
Ja <Ja@DESKTOP-SVD375N>
2017-09-25 22:48:33 +0200

    Fix minor bugs.

--- GomokuBot/GomokuBot/Input/CommandListener.py
+++ GomokuBot/GomokuBot/Input/CommandListener.py
@@ -2,10 +2,11 @@ import threading
 from Input.InputReader import InputReader
 class CommandListener(object):
     def __init__(self):
-        oListenerThread = threading.start_new_thread(target=self.listenerLoop)
-        oListenerThread.start()
+        oListenerThread = threading.Thread(target=self.listenerLoop)
+        
         self.oInput = InputReader()
         self.Flag = False
+        oListenerThread.start()
     def listenerLoop(self):
         while(True):
             self.oInput.waitForInput(32)
@@ -13,9 +14,11 @@ class CommandListener(object):
             print("Mouse Flag is set to",self.Flag)
 __oCommandListener = None
 def getCommandListener():
+    global __oCommandListener
     if(__oCommandListener is None):
         __oCommandListener = CommandListener()
     return __oCommandListener
+
 
 
 
--- GomokuBot/GomokuBot/Input/MouseClicker.py
+++ GomokuBot/GomokuBot/Input/MouseClicker.py
@@ -1,4 +1,4 @@
-from win32 import win32api
+from win32 import win32api, win32gui
 import win32con
 import time
 from Input.CommandListener import *
@@ -8,15 +8,17 @@ class MouseClicker(object):
         self.oCommandListener = getCommandListener()
         self.lastPos = (0,0)
     def click(self, iX, iY):
+        iX = int(iX)
+        iY = int(iY)
         if(self.oCommandListener.Flag is False):
             flags, hcursor, (x, y) = win32gui.GetCursorInfo()
             if(self.lastPos != (x, y)):
                 print("If you want to block mouse moving press SPACE!")
-            self.lastPos = (x, y)
-            win32api.SetCursorPos((x,y))
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0)
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0)
-            print("Mouse click at",x,y)
+            self.lastPos = (iX, iY)
+            win32api.SetCursorPos((iX,iY))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,iX,iY,0,0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,iX,iY,0,0)
+            print("Mouse click at",iX,iY)
             return True
         else:
             print("Tried to click at",x,y)
