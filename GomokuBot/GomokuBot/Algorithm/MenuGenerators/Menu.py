bcbbb09d8dfb6f69e23e6ae3fa48c119e3a512d4
Marcin Pawlikowski <kojoten95@gmail.com>
2017-09-25 16:34:48 +0200

    Add relativity to Menu class.

--- GomokuBot/GomokuBot/Algorithm/MenuGenerators/Menu.py
+++ GomokuBot/GomokuBot/Algorithm/MenuGenerators/Menu.py
@@ -1,6 +1,13 @@
 class Menu(object):
     positions = {}
-    def __init__(self, tiPlayer1Position, tiPlayer2Position, tiStartPosition, bIsRelative):
+    def __init__(self, tiPlayer1Position, tiPlayer2Position, tiStartPosition, bIsRelative=False, oCoordinates=(0,0,0,0)):
+        if(bIsRelative is True):
+            tiPlayer1Position[0]+=oCoordinates[0]
+            tiPlayer1Position[1]+=oCoordinates[1]
+            tiPlayer2Position[0]+=oCoordinates[0]
+            tiPlayer2Position[1]+=oCoordinates[1]
+            tiStartPosition[0]+=oCoordinates[0]
+            tiStartPosition[1]+=oCoordinates[1]
         self.positions["player1Position"] = tiPlayer1Position
         self.positions["player2Position"] = tiPlayer2Position
         self.positions["startPosition"] = tiStartPosition
