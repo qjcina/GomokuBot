708e41ff8a0ca7727901a0d5753f4adbe319c780
Ja <Ja@DESKTOP-SVD375N>
2017-09-25 22:47:28 +0200

    Update Colors.

--- GomokuBot/GomokuBot/Resources/Colors.py
+++ GomokuBot/GomokuBot/Resources/Colors.py
@@ -2,7 +2,9 @@ from enum import Enum
 class Colors(Enum):
     ButtonBorder = [143, 143, 143]
     EmptySeat = [238, 238, 238]
-    NonEmptySeat = [255, 255, 255]
+    NonEmptySeat = [255, 255, 255] #also Player2
+    SeatHeader = [31, 41, 48]
+    Player1 = [102, 102, 102]
 
 def getColor(iColor):
     iColor = list(iColor)
