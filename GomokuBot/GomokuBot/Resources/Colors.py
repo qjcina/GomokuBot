fbb9eff7affdac1f6b4030dba5d3b96ad5ced14c
Marcin Pawlikowski <kojoten95@gmail.com>
2017-09-26 14:59:17 +0200

    Add more colors.

--- GomokuBot/GomokuBot/Resources/Colors.py
+++ GomokuBot/GomokuBot/Resources/Colors.py
@@ -5,11 +5,15 @@ class Colors(Enum):
     NonEmptySeat = [255, 255, 255] #also Player2
     SeatHeader = [31, 41, 48]
     Player1 = [102, 102, 102]
-
+    Background = [240, 176, 96]
+    LastMove = [255, 0, 0]
+    Black = [0, 0, 0]
+    NotFound = [-1, -1, -1]
 def getColor(iColor):
     iColor = list(iColor)
     for oSingleColor in Colors:
         if(iColor==oSingleColor.value):
             return oSingleColor.name
+    return Colors.NotFound.name
 
 
